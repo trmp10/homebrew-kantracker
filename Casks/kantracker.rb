931f62310359a52d9494759b0362f3586b70216b
toni <paduatoni@gmail.com>
2026-06-05 13:12:02 +0800

    Update KanTracker cask to v1.4

--- Casks/kantracker.rb
+++ Casks/kantracker.rb
@@ -1,6 +1,6 @@
 cask "kantracker" do
-  version "1.3"
-  sha256 "c9d422cc43fc38683948f7cfa19696c5229e979940ec71a7acc75a6fae552b1a"
+  version "1.4"
+  sha256 "40a995d144a8f6af497115369dbfdb029cd4ac4cd9c13478d0de60dcb0e56daf"
 
   url "https://github.com/trmp10/kantracker/releases/download/v#{version}/KanTracker.zip"
   name "KanTracker"
